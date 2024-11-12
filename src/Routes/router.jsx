a3e7a877121c41b4b097f023d71b22c77ebe0eb7
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-12 10:29:01 +0530

    update

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,11 +1,20 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Events from "../Pages/Events";
+import Contact from "../Pages/Contact";
 
 function router() {
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default router
+export default router;
