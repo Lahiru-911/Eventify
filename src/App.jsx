ba71f59d1b15d0739a19ab49b80c63a958aaf499
Lahiru Kavinda <lahiruxk@gmail.com>
2024-11-12 11:10:01 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import AppRouter from "./Routes/router";
-import "./App.css";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Router>
           <NavBar />
           <AppRouter />
-          </Router>
+        </Router>
       </NextUIProvider>
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 const Home = () => {
   return <>
-  
+  <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate ullam aspernatur optio numquam illum soluta vero magni? Laudantium doloribus voluptatem consequatur praesentium repellendus sunt eius eum! Hic illo nostrum amet?</div>
+  <img src="https://corp.kaltura.com/wp-content/uploads/2023/11/Events-Sessions.png" alt="" />
   </>;
 };
 
