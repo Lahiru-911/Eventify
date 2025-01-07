c2be22a6e81eedd848b6f81a20d0f067dd4429a7
Lahiru Kavinda <lahiruxk@gmail.com>
2025-01-07 23:30:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import AppRouter from "./Routes/router";
-import Ths from "./Components/NavBar/ths";
-import EventGrid from "./Components/EventGrid";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
@@ -14,8 +12,6 @@ function App() {
         <Router>
           <NavBar />
           <AppRouter />
-          {/* <Ths/> */}
-          {/* <EventGrid/> */}
           <Footer />
         </Router>
       </NextUIProvider>
