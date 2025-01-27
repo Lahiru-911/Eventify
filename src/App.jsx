e63a5224662081b0c7beb8a2ac75663cbc2e1bf0
Lahiru Kavinda <lahiruxk@gmail.com>
2025-01-27 22:58:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <NextUIProvider>
         <Router>
           <NavBar />
-          {/* <AppRouter /> */}
+          <AppRouter />
           <Footer />
           {/* <AddEventForm/> */}
         </Router>
